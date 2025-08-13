f23ddc96688af1681ae08b71783a8161a2e54432
ayushpatidar814 <ayushpatidar814@gmail.com>
2025-08-13 18:49:41 +0530

    bug fixed

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const serverless = require("serverless-http");
 const connectDB = require("./config/db.js")
 const authRoutes = require("./routes/authRoutes.js")
 const incomeRoutes = require("./routes/incomeRoutes.js")
@@ -9,7 +10,6 @@ const expenseRoutes = require("./routes/expenseRoutes.js")
 const dashboardRoutes = require("./routes/dashboardRoutes.js")
 
 const app = express();
-connectDB();
 
 // Middleware to handle CORS
 app.use(
@@ -17,14 +17,12 @@ app.use(
         origin: process.env.CLIENT_URL || "*",
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["content-type", "Authorization"],
+        credentials: true,
     })
 );
 
 app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.send("🚀 Welcome Mr. Ayush");
-  });
+connectDB();
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/income", incomeRoutes);
@@ -34,6 +32,17 @@ app.use("/api/v1/dashboard", dashboardRoutes);
 // Serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
+app.get("/", (req, res) => {
+    res.send("🚀 Welcome Mr. Ayush");
+  });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
+
+if (process.env.VERCEL) {
+    const serverless = require("serverless-http");
+    module.exports = serverless(app);
+  } else {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  }
